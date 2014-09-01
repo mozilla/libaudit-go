19a54ca8b92e77906ff55c7f494ba7e3695a64f9
aalekh <aalekh1993@rediffmail.com>
2014-09-02 01:36:53 +0530

    Everything except inserting println complete

--- main.go
+++ main.go
@@ -1,16 +1,38 @@
 package main
 
-import . "syscall"
-import . "encoding/binary"
-import "fmt"
-import . "unsafe"
+import (. "syscall"
+         "encoding/binary"
+        "fmt"
+        "unsafe"
+        . "sync/atomic"
+        "errors"
+        "net"
+)        
+
+var nextSeqNr uint32
 
 type NetlinkAuditRequest struct {
     Header NlMsghdr
     Data   RtGenmsg
 }
 
-func (msg *IfInfomsg) ToWireFormat() []byte {
+var (
+    ErrWrongSockType = errors.New("Wrong socket type")
+    ErrShortResponse = errors.New("Got short response from netlink")
+)
+// A Route is a subnet associated with the interface to reach it.
+type Route struct {
+    *net.IPNet
+    Iface *net.Interface
+    Default bool
+}
+
+
+type IfInfomsg_two struct {
+    IfInfomsg
+}
+/*
+func (msg *IfInfomsg_two) ToWireFormat() []byte {
     native := nativeEndian()
     length := SizeofIfInfomsg
     b := make([]byte, length)
@@ -22,13 +44,37 @@ func (msg *IfInfomsg) ToWireFormat() []byte {
     native.PutUint32(b[12:16], msg.Change)
     return b
 }
+*/
 
-func nativeEndian() ByteOrder {
-    var x uint32 = 0x01020304
-    if *(*byte)(Pointer(&x)) == 0x01 {
-        return BigEndian
+
+func (rr *NetlinkRequest) ToWireFormat() []byte {
+    native := nativeEndian()
+    length := rr.Len
+    dataBytes := make([][]byte, len(rr.Data))
+    for i, data := range rr.Data {
+        dataBytes[i] = data.ToWireFormat()
+        length += uint32(len(dataBytes[i]))
     }
-    return LittleEndian
+    b := make([]byte, length)
+    native.PutUint32(b[0:4], length)
+    native.PutUint16(b[4:6], rr.Type)
+    native.PutUint16(b[6:8], rr.Flags)
+    native.PutUint32(b[8:12], rr.Seq)
+    native.PutUint32(b[12:16], rr.Pid)
+    next := 16
+    for _, data := range dataBytes {
+        copy(b[next:], data)
+        next += len(data)
+    }
+    return b
+}
+
+func nativeEndian() binary.ByteOrder {
+    var x uint32 = 0x01020304
+    if *(*byte)(unsafe.Pointer(&x)) == 0x01 {
+        return binary.BigEndian
+    }
+    return binary.LittleEndian
 }
 
 /*
@@ -43,18 +89,13 @@ func newNetlinkAuditRequest(proto, seq, family int) []byte {
 }
 */
 
-type IfInfomsg struct {
-    IfInfomsg
-}
 
 type NetlinkRequest struct {
     NlMsghdr
     Data []NetlinkRequestData
 }
 
-func (msg *IfInfomsg) Len() int {
-    return SizeofIfInfomsg
-}
+
 
 func (s *NetlinkSocket) Close() {
     Close(s.fd)
@@ -73,7 +114,8 @@ type NetlinkSocket struct {
 func getNetlinkSocket() (*NetlinkSocket, error) {
     fd, err := Socket(AF_NETLINK, SOCK_RAW, NETLINK_AUDIT)
     if err != nil {
-        return nil, err
+        //return nil, err
+        fmt.Println("statement 1")
     }
     s := &NetlinkSocket{
         fd: fd,
@@ -88,8 +130,10 @@ func getNetlinkSocket() (*NetlinkSocket, error) {
 
 func (s *NetlinkSocket) Send(request *NetlinkRequest) error {
     if err := Sendto(s.fd, request.ToWireFormat(), 0, &s.lsa); err != nil {
+        fmt.Println("statement error 1")
         return err
     }
+    fmt.Println("statement 2")
     return nil
 }
 
@@ -97,22 +141,27 @@ func (s *NetlinkSocket) Receive() ([]NetlinkMessage, error) {
     rb := make([]byte, Getpagesize())
     nr, _, err := Recvfrom(s.fd, rb, 0)
     if err != nil {
+        fmt.Println("statement error 2")
         return nil, err
     }
     if nr < NLMSG_HDRLEN {
+        fmt.Println("statement error 3")
         return nil, ErrShortResponse
     }
     rb = rb[:nr]
+    fmt.Println("statement 3")
     return ParseNetlinkMessage(rb)
 }
 
 func (s *NetlinkSocket) GetPid() (uint32, error) {
     lsa, err := Getsockname(s.fd)
     if err != nil {
+        fmt.Println("statement 4")
         return 0, err
     }
     switch v := lsa.(type) {
     case *SockaddrNetlink:
+        fmt.Println("statement 5")
         return v.Pid, nil
     }
     return 0, ErrWrongSockType
@@ -123,6 +172,7 @@ func (s *NetlinkSocket) HandleAck(seq uint32) error {
     native := nativeEndian()
     pid, err := s.GetPid()
     if err != nil {
+        fmt.Println("statement error 4")
         return err
     }
     done:
@@ -133,12 +183,14 @@ func (s *NetlinkSocket) HandleAck(seq uint32) error {
             }   
             for _, m := range msgs {
                 if m.Header.Seq != seq {
-                    return fmt.Errorf("Wrong Seq nr %d, expected %d", m.Header.Seq, seq)
+                    fmt.Println("Wrong Seq, expected ")
                 }
                 if m.Header.Pid != pid {
-                    return fmt.Errorf("Wrong pid %d, expected %d", m.Header.Pid, pid)
+                    fmt.Println("Wrong pid , expected")
                 }
                 if m.Header.Type == NLMSG_DONE {
+                    //fmt.Println("working")
+                    fmt.Println("statement 6")
                     break done
                 }
                 if m.Header.Type == NLMSG_ERROR {
@@ -146,6 +198,7 @@ func (s *NetlinkSocket) HandleAck(seq uint32) error {
                     if error == 0 {
                         break done
                     }
+                    fmt.Println(EINVAL)
                     return Errno(-error)
                 }
             }
@@ -153,6 +206,23 @@ func (s *NetlinkSocket) HandleAck(seq uint32) error {
     return nil
 }
 
-func main() {
-    
+func newNetlinkRequest(proto, flags int) *NetlinkRequest {
+    return &NetlinkRequest{
+        NlMsghdr: NlMsghdr{
+            Len: uint32(NLMSG_HDRLEN),
+            Type: uint16(proto),
+            Flags: NLM_F_REQUEST | uint16(flags),
+            Seq: AddUint32(&nextSeqNr, 1),
+        },
+    }
 }
+
+func main() {
+    socket := getNetlinkSocket()
+    defer Close(socket)
+    RequestData := newNetlinkRequest(AF_NETLINK, NETLINK_AUDIT)
+    socketFamily := NetlinkSocket{ AF_NETLINK,socket}
+    socket.Send(socketFamily)
+    socket.HandleAck(RequestData.NlMsghdr.Seq)
+}
+        
