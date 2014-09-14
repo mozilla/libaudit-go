c5c0f86c726ee3badac040b85df24b9b289765b5
Hardik Juneja <hardikjuneja.hj@gmail.com>
2014-09-14 16:16:35 +0530

    adding our own parsenetlink function

--- netlink_old.go
+++ netlink_old.go
@@ -13,7 +13,7 @@ const (
 
 type NetlinkAuditRequest struct {
     Header s.NlMsghdr
-    Data   string
+    Data   []byte //string
 }
 
 type AuditReply struct {
@@ -52,6 +52,32 @@ func nlmAlignOf(msglen int) int {
     return (msglen + s.NLMSG_ALIGNTO - 1) & ^(s.NLMSG_ALIGNTO - 1)
 }
 
+func ParseAuditNetlinkMessage(b []byte) ([]NetlinkAuditRequest, error) {
+    var msgs []NetlinkAuditRequest
+    for len(b) >= s.NLMSG_HDRLEN {
+        h, dbuf, dlen, err := netlinkMessageHeaderAndData(b)
+        if err != nil {
+            fmt.Println("Error in parse audit")
+            return nil, err
+        }
+        m := NetlinkAuditRequest{Header: *h, Data: dbuf[:int(h.Len)-s.NLMSG_HDRLEN]}
+        msgs = append(msgs, m)
+        b = b[dlen:]
+    }
+    return msgs, nil
+}
+
+func netlinkMessageHeaderAndData(b []byte) (*s.NlMsghdr, []byte, int, error) {
+    h := (*s.NlMsghdr)(unsafe.Pointer(&b[0]))
+    //fmt.Println(int(s.NLMSG_HDRLEN));
+    if int(h.Len) < s.NLMSG_HDRLEN || int(h.Len) > len(b) {
+        fmt.Println("Error Here")
+        fmt.Println(s.NLMSG_HDRLEN, h.Len, h.Len, len(b))
+        return nil, nil, 0, s.EINVAL
+    }
+    return h, b[s.NLMSG_HDRLEN:], nlmAlignOf(int(h.Len)), nil
+}
+
 func main() {
 
     // Create a netlink socket
@@ -71,7 +97,7 @@ func main() {
     }
     fmt.Println("**** Starting Netlink")
 
-    wb := newNetlinkAuditRequest(1002, 1, s.NETLINK_AUDIT)
+    wb := newNetlinkAuditRequest(1005, 1, s.NETLINK_AUDIT)
 
     //TODO: try sendmsg here 
     if err := s.Sendto(sock, wb, 0, lsa); err != nil {
@@ -83,14 +109,13 @@ func main() {
 done:
     for {
 
-        // use Audit_reply here
-        // r := make([]byte, s.Getpagesize())
-        
+        // r := make([]byte, s.Getpagesize())     
         r := newNetlinkAuditReply();
-        //fmt.Println(r);
 
-        //nr, _, err := Recvfrom(s, rb, MSG_PEEK)
-        nr, _, err := s.Recvfrom(sock, r, s.MSG_PEEK|s.MSG_DONTWAIT)
+        //nr, _, err := s.Recvfrom(sock, r, s.MSG_PEEK|s.MSG_DONTWAIT)
+        nr, _, err := s.Recvfrom(sock, r, 0)
+
+        //r = r[s.NLMSG_HDRLEN:nr]
         if err != nil {
             fmt.Println("rec err: ", err)
             return
@@ -100,9 +125,10 @@ done:
             fmt.Println("nr < nlmsg hdrlen")
             return
         }
+
         r = r[:nr]
         tab = append(tab, r...)
-        msgs, err := s.ParseNetlinkMessage(r)
+        msgs, err := ParseAuditNetlinkMessage(r)
 
         if err != nil {
             fmt.Println("parse error:", err)
@@ -111,7 +137,7 @@ done:
 
         for _, m := range msgs {
 
-            fmt.Println(m.Header.Pid)
+            fmt.Println(string(m.Data))
  
             lsa, err := s.Getsockname(sock)
             if err != nil {
@@ -143,8 +169,9 @@ done:
 
         }
         
-        //fmt.Println(string(tab[:]));
-        a,_ := s.ParseNetlinkMessage(tab)
-        fmt.Println(a)
+        fmt.Println(tab[:]);
+        //a,_ := s.ParseNetlinkMessage(tab)
+        //fmt.Println(a)
     }
+    fmt.Println(tab);
 }
