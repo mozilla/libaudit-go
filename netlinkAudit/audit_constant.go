42f15e936c13d1833af4869e5a328d8ff95e2112
arunk-s <arunsori94@gmail.com>
2014-10-04 23:47:31 +0530

    Added AuditAddRuleData(), No testing though

---
+++ netlinkAudit/audit_constant.go
@@ -0,0 +1,16 @@
+package netlinkAudit
+
+const (
+	MAX_AUDIT_MESSAGE_LENGTH = 8960
+	AUDIT_GET                = 1000
+	AUDIT_SET                = 1001 /* Set status (enable/disable/auditd) */
+	AUDIT_LIST               = 1002
+	AUDIT_LIST_RULES         = 1013
+	AUDIT_ADD_RULE           = 1011 /* Add syscall filtering rule */
+	AUDIT_FIRST_USER_MSG     = 1100 /* Userspace messages mostly uninteresting to kernel */
+	AUDIT_MAX_FIELDS         = 64
+	AUDIT_BITMASK_SIZE       = 64
+	AUDIT_GET_FEATURE        = 1019
+	AUDIT_STATUS_ENABLED     = 0x0001
+	AUDIT_FILTER_ENTRY       = 0x02 /* Apply rule at syscall entry */
+)
