29c7b152c2971dd1c487444f8639eb2a294426de
arunk-s <arunsori94@gmail.com>
2014-10-11 02:14:09 +0530

    Fix for fields ; More constants

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/arunk-s/netlinkAudit" //Should be changed according to individual settings
+	//	"syscall"
 	///	"unsafe"
 )
 
@@ -21,8 +22,6 @@ func main() {
 		fmt.Println("Horrah")
 	}
 	var foo netlinkAudit.AuditRuleData
-
-	//NO Fields for now
 	// we need audit_name_to_field( ) && audit_rule_fieldpair_data
 	//Syscall rmdir() is 84 on table
 	//fmt.Println(unsafe.Sizeof(foo))
@@ -30,10 +29,17 @@ func main() {
 	//fmt.Println(foo)
 	foo.Fields[foo.Field_count] = netlinkAudit.AUDIT_ARCH
 	foo.Fieldflags[foo.Field_count] = netlinkAudit.AUDIT_EQUAL
-	foo.Values[foo.Field_count] = 64
+	foo.Values[foo.Field_count] = netlinkAudit.AUDIT_ARCH_X86_64
 	foo.Field_count++
-
+	//seq := 3
 	netlinkAudit.AuditAddRuleData(s, &foo, netlinkAudit.AUDIT_FILTER_EXIT, netlinkAudit.AUDIT_ALWAYS)
+	//Listening in a while loop from kernel when some event goes down through Kernel
+	/*
+		for {
+			netlinkAudit.AuditGetReply(s, syscall.Getpagesize(), syscall.MSG_DONTWAIT, seq)
+			seq++
+		}
+	*/
 	//auditctl -a rmdir exit,always
 	//Flags are exit
 	//Action is always
